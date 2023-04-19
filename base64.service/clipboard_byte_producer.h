dcc8612dda16dac44a7d6e3514ee71483c3f118e
Terry Moreland <tsmoreland@live.com>
2023-04-20 00:48:56 +0100

    formatting

--- base64.service/clipboard_byte_producer.h
+++ base64.service/clipboard_byte_producer.h
@@ -1,15 +1,18 @@
 //
 // Copyright (c) 2023 Terry Moreland
-// Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), 
-// to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, 
-// and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+// Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
+// documentation files (the "Software"), to deal in the Software without restriction, including without limitation the
+// rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to
+// permit persons to whom the Software is furnished to do so, subject to the following conditions:
 //
-// The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+// The above copyright notice and this permission notice shall be included in all copies or substantial portions of the
+// Software.
 //
-// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
-// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, 
-// WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE
+// WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+// COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
+// OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+//
 
 #pragma once
 
@@ -18,22 +21,18 @@
 #include "../modern_win32_api.user/clipboard_concept.h"
 #include "../modern_win32_api.user/clipboard_traits.h"
 
-namespace moreland::base64::service
-{
+namespace moreland::base64::service {
     template <modern_win32_api::user::Clipboard CLIPBOARD = modern_win32_api::user::clipboard_traits>
-    class clipboard_byte_producer final : public converters::byte_producer
-    {
+    class clipboard_byte_producer final : public converters::byte_producer {
     public:
         explicit clipboard_byte_producer() = default;
 
-        [[nodiscard]]
-        std::optional<std::vector<unsigned char>> chunk_or_empty() override
-        {
-            return std_extensions::map<std::string, std::vector<unsigned char>>(CLIPBOARD::get_clipboard(), 
-                [](auto const& original)  {
+        [[nodiscard]] std::optional<std::vector<unsigned char>> chunk_or_empty() override {
+            return std_extensions::map<std::string, std::vector<unsigned char>>(
+                CLIPBOARD::get_clipboard(), [](auto const& original) {
                     return std::vector<unsigned char>(std::begin(original), std::end(original));
                 });
         }
     };
 
-}
+} // namespace moreland::base64::service
