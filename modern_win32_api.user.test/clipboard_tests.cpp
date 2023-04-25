a05e1c762d2a7392251544e868fa1a22316b48b3
Terry Moreland <tsmoreland@live.com>
2023-04-25 23:56:06 +0100

    formatting

--- modern_win32_api.user.test/clipboard_tests.cpp
+++ modern_win32_api.user.test/clipboard_tests.cpp
@@ -1,25 +1,26 @@
 //
 // Copyright (c) 2023 Terry Moreland
-// Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), 
-// to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, 
-// and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+// Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
+// documentation files (the "Software"), to deal in the Software without restriction, including without limitation the
+// rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to
+// permit persons to whom the Software is furnished to do so, subject to the following conditions:
 //
-// The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+// The above copyright notice and this permission notice shall be included in all copies or substantial portions of the
+// Software.
 //
-// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
-// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, 
-// WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE
+// WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+// COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
+// OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+//
 
 #include "pch.h"
 #include "../modern_win32_api.user/clipboard.h"
 
 using std::string;
 
-namespace modern_win32_api::user::tests
-{
-    BOOST_AUTO_TEST_CASE(set_clipboard__returns_true__when_have_desktop_access)
-    {
+namespace modern_win32_api::user::tests {
+    BOOST_AUTO_TEST_CASE(set_clipboard__returns_true__when_have_desktop_access) {
         string const data = "sample-text";
 
         auto const actual = set_clipboard(data, GetDesktopWindow());
@@ -27,8 +28,7 @@ namespace modern_win32_api::user::tests
         BOOST_TEST(actual);
     }
 
-    BOOST_AUTO_TEST_CASE(get_clipboard__returns_set_value__when_set_clipboard_used)
-    {
+    BOOST_AUTO_TEST_CASE(get_clipboard__returns_set_value__when_set_clipboard_used) {
         string const expected = "sample-text";
         static_cast<void>(set_clipboard(expected));
 
@@ -37,4 +37,4 @@ namespace modern_win32_api::user::tests
         BOOST_TEST(actual == expected);
     }
 
-}
+} // namespace modern_win32_api::user::tests
